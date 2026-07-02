c037e22f3f612afdf7aeda63adf91dacf054ebb9
gichinga03 <gichinga03@gmail.com>
2026-07-02 10:34:48 +0300

    first commit

---
+++ src/main/java/com/greenharvest/greenharvest/GreenharvestApplication.java
@@ -0,0 +1,13 @@
+package com.greenharvest.greenharvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenharvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenharvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenharvest/greenharvest/GreenharvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenharvest.greenharvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenharvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
